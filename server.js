8614802e082c881592ba6cffcb5417c4cc3dc1f8
Nishant saini <ns0109375@gmail.com>
2024-02-25 16:42:27 +0530

    error handling

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-    res.send({ name: "nishant" , branch:"cse"  });
+    res.send({ name: "nishant", branch: "cse" });
 });
 app.get("/api/products", (req, res) => {
-    res.send({ name: "iphone 15" , prize: 85000 , rating:4.5 });
+    res.send({ name: "iphone 15", prize: 85000, rating: 4.5 });
+});
+
+app.use((req, res, next) => {
+    console.log("1st");
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    if (req.accepts("json")) {
+        res.json({ error: err.message });
+        return;
+    }
 });
